5fcebfd115eac18f7458994ed632f5106714926d
sevak19 <tu1227csn@gmail.com>
2024-10-05 14:05:24 -0300

    Upload NavBarADM

--- codigo/public/assets/js/login.js
+++ codigo/public/assets/js/login.js
@@ -131,10 +131,40 @@ function showUserInfo (element) {
     var elemUser = document.getElementById(element);
     if (elemUser) {
         if (usuarioCorrente.ehADM == 1) {
-            elemUser.innerHTML += `<a href="" style="margin: 50px;">ADM</a>`;
+            elemUser.innerHTML += `<a onclick="navADM()" style="margin: 50px;"><i class="ph-bold ph-clipboard-text" style="font-size: 23px"></i></a>`;
         }
         elemUser.innerHTML += `${usuarioCorrente.nome} (${usuarioCorrente.login}) 
         <a onclick="logoutUser()">❌</a>`;
+    }
+}
+
+function navADM () {
+    var elemUser = document.getElementById("navBarADM");
+    var admMenu = document.getElementById("admNavMenu");
+    
+    if (admMenu) {
+        // Se o menu já existe, remove-o (toggle off)
+        admMenu.remove();
+    } else {
+        elemUser.innerHTML += `<nav id="admNavMenu" class="navbar navbar-expand-md navbar-light bg-light">
+                                    <a class="navbar-brand" href="">MENU ADM</a>
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse" id="navbarNav1">
+                                        <ul class="navbar-nav">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="forum.html">Cadastro Artigos</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="dias-sobrios.html">Cadastro Clinicas</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="perfil.html">Cadastro 3</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>`;
     }
 }
 
